68bb65e306de67a964208c05f370e01a3c7ed299
Jose Montes De Oca Morfin <112668683+JoseMDO@users.noreply.github.com>
2023-12-27 12:50:29 +0000

    got lat and lng working

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,39 @@
-pp "howdy"
+require "http"
+require "json"
+
+pp "Where are you located"
+
+user_location = gets.chomp.gsub(" ", "%20")
+# user_location = "Chicago"
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+parsed_response = JSON.parse(raw_response)
+
+
+lat =  parsed_response.fetch("results")[0].fetch("geometry").fetch("location").fetch("lat")
+lng =  parsed_response.fetch("results")[0].fetch("geometry").fetch("location").fetch("lng")
+
+pp lat, lng
+
+# pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+# pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+
+# raw_response = HTTP.get(pirate_weather_url)
+
+# require "json"
+
+# parsed_response = JSON.parse(raw_response)
+
+# currently_hash = parsed_response.fetch("currently")
+
+# current_temp = currently_hash.fetch("temperature")
+
+# puts "The current temperature is " + current_temp.to_s + "."
